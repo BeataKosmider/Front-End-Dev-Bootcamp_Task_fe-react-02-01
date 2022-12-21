70d9e023918b583a85471689b694958c2bfb00ab
Krzysztof Mendrek <kmendrek94@gmail.com>
2022-12-21 08:23:10 +0100

    Task update

--- src/SquareOrCircle.js
+++ src/SquareOrCircle.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-function App() {
+function SquareOrCircle() {}
 
-}
-
-export default App;
+export default SquareOrCircle;
--- src/SquareOrCircle.test.js
+++ src/SquareOrCircle.test.js
@@ -5,20 +5,27 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('Button tests', () => {
-    let wrapper = shallow(<SquareOrCircle />);
+describe('Component tests', () => {
+  let wrapper = shallow(<SquareOrCircle />);
 
-    it('Dodano poprawnie komponent SquareOrCircle', () => {
-      expect(!!wrapper).toBe(true);
-    });
-    it('SquareOrCircle ma domyślnie szerokość 200px', () => {
-      expect(wrapper.find('div').prop('style')).toHaveProperty('width', '200px');
-    });
-    it('SquareOrCircle ma domyślnie wysokość 200px', () => {
-      expect(wrapper.find('div').prop('style')).toHaveProperty('height', '200px');
-    });
-    it('Dodano parametr który przy wartości true sprawia, że element staje się okręgiem', () => {
-      wrapper = shallow(<SquareOrCircle isCircle={true} />);
-      expect(wrapper.find('div').prop('style')).toHaveProperty('borderRadius', '50%');
-    });
+  it('Dodano poprawnie komponent SquareOrCircle', () => {
+    expect(!!wrapper).toBe(true);
   });
+  it('SquareOrCircle ma domyślnie szerokość 200px', () => {
+    expect(wrapper.find('div').prop('style')).toHaveProperty('width', '200px');
+  });
+  it('SquareOrCircle ma domyślnie wysokość 200px', () => {
+    expect(wrapper.find('div').prop('style')).toHaveProperty('height', '200px');
+  });
+  it('Dodano prop isCircle który przy wartości true ustawia borderRadius na 50%', () => {
+    wrapper = shallow(<SquareOrCircle isCircle={true} />);
+    expect(wrapper.find('div').prop('style')).toHaveProperty(
+      'borderRadius',
+      '50%'
+    );
+  });
+  it('Dodano prop isCircle który przy wartości false ustawia borderRadius na 0', () => {
+    wrapper = shallow(<SquareOrCircle isCircle={false} />);
+    expect(wrapper.find('div').prop('style')).toHaveProperty('borderRadius', 0);
+  });
+});
