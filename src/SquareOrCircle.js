311e7bca4da4775ebc37971db2d6d1b5756a63bc
Beata Kosmider <kosmibe@gmail.com>
2023-03-15 11:59:01 +0100

    task is done

--- src/SquareOrCircle.js
+++ src/SquareOrCircle.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+let dynamicStyles = {
+  width: "200px",
+  height: "200px",
+  borderRadius: "50%",
+};
 
 function App() {
-
+  return <div style={dynamicStyles}></div>;
 }
 
 export default App;
